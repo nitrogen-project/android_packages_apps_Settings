a69b902130a0b2702527c9f635af9ae152ed222c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-12-09 14:47:57 +0000

    Expanded Desktop tile longpress action
    Change-Id: If6f00736d07580f518cbbcd1ec1a377174d25728
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -61,6 +61,7 @@ import com.android.settings.accounts.ChooseAccountActivity;
 import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AdvancedAppSettings;
 import com.android.settings.applications.DrawOverlayDetails;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -364,6 +365,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
             PulseSettings.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 
