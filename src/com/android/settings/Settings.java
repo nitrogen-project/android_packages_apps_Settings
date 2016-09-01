5f824f68a15a1ed1e8933be22fb8b9f5f35fd2a3
xyyx <xyyx@mail.ru>
2016-09-01 16:49:50 +0800

    Build NitrogenSettings with Settings. Thanks to @beanstown106, @Mazda, @maxwen

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -158,4 +158,5 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+    public static class NitrogenSettingsActivity extends SettingsActivity { /* empty */ }
 }
