13d05d9ac577709d2fa50eb6adf8ccad04623ff3
Valeriy <mexanicteam@gmail.com>
2016-09-08 00:13:22 +0400

    Settings: Added Nitrogen logo and changelog

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -65,6 +65,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
+    private static final String KEY_NOS_CHANGELOG = "nos_changelog";
+    private static final String KEY_NOS_LOGO = "nos_logo";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -113,6 +115,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(DeviceInfoUtils.getFormattedKernelVersion());
+        // Nitrogen
+        findPreference(KEY_NOS_CHANGELOG).setEnabled(true);
+        findPreference(KEY_NOS_LOGO).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
