7189cca162698ace8e0b641e23980a776cca228c
ezio84 <brabus84@gmail.com>
2017-09-03 19:22:05 +0200

    Allow to doubletap or longpress power to toggle flashlight [3/3]
    Change-Id: I9b0b3dd2f46a331faf88ee43bf5fc3d47fd3992c
    Signed-off-by: xyyx <xyyx@mail.ru>

--- src/com/android/settings/gestures/DoubleTapPowerPreferenceController.java
+++ src/com/android/settings/gestures/DoubleTapPowerPreferenceController.java
@@ -74,6 +74,14 @@ public class DoubleTapPowerPreferenceController extends GesturePreferenceControl
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean enabled = (boolean) newValue;
         Settings.Secure.putInt(mContext.getContentResolver(), SECURE_KEY, enabled ? ON : OFF);
+        int previousTorchPref = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0);
+        if (enabled && (previousTorchPref == 1)) {
+            //if double tap for torch was active and we enable here double tap for camera,
+            //set torch action to long press mode
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 2);
+        }
         return true;
     }
 
